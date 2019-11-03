923c73ff77a8cc16c0fd91a9d6a2b6eee05e7a90
noisyfox <timemanager.rick@gmail.com>
2019-11-03 22:15:18 +1100

    Fix issue in GetProcessPathByWindow()

--- ACT.FFXIVPing/ACT.FFXIVPing/Properties/AssemblyInfo.cs
+++ ACT.FFXIVPing/ACT.FFXIVPing/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("ACT.FFXIVPing")]
-[assembly: AssemblyCopyright("Copyright © Noisyfox 2017")]
+[assembly: AssemblyCopyright("Copyright © Noisyfox 2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -51,5 +51,5 @@ using System.Windows;
 // 可以指定所有值，也可以使用以下所示的 "*" 预置版本号和修订号
 // 方法是按如下所示使用“*”: :
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.2.0.0")]
-[assembly: AssemblyFileVersion("1.2.0.0")]
+[assembly: AssemblyVersion("1.2.1.0")]
+[assembly: AssemblyFileVersion("1.2.1.0")]
